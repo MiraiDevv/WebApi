8654d9a00d47f33d1ba70b4a62014ff51f27c9bf
Mirai <mirai.dev@outlook.com>
2024-11-26 14:05:15 -0300

    Initial Commit

---
+++ data/AppDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApi9.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
